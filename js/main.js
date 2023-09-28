2dd871eb5f38909672f62900744fdbedcfb25700
eliezer <eliezer.santos1010@gmail.com>
2023-09-28 05:58:55 -0300

    nova repositorio

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+	$('#carroussel-imagens').slick({
+		autoplay:true,
+	});
+
+	$('.menu-hamburguer').click(function() {
+		$('nav').slideToggle();
+	})
+
+	$('#telefone').mask('(00) 00000-0000')
+
+	$('form').validate({
+		rules: {
+			nome: {
+				required: true
+			},
+			email: {
+				required: true,
+				email:true
+			},
+			telefone: {
+				required: true
+			},
+			veiculoDeInteresse: {
+				required:false
+			},
+			mensagem: {
+				required: true
+			}
+		},
+		messages: {
+			nome: 'Por favor, insira seu nome',
+			email: 'Por favor insira seu E-mail',
+			telefone: 'Por favor insira seu Telefone'
+		},
+		submitHandler: function(form) {
+			console.log(form)
+		},
+		invalidHandler: function(evento,validador) {
+			let camposincorretos = validador.numberOfInvalids();
+			if (camposincorretos) {
+				alert(` Existem ${camposincorretos} campos incorretos`)
+			}
+		}
+	})
+	$('.lista-veiculos button').click(function() {
+		const destino = $('#contato');
+
+		const nomeVeiculo = $(this).parent().find('h3').text();
+
+		$('#veiculo-interesse').val(nomeVeiculo);
+
+		$('html').animate({
+			scrollTop: destino.offset().top
+		},1000)
+
+
+	})
+})
